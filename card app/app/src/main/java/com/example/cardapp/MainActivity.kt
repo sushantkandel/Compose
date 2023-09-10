4fdd8958d6fe63c9e76a52116f3512e1b45ddd33
VAI <sushant.kandel@swifttech.com.np>
2023-09-10 10:34:18 +0545

    component devider and text field is used

--- card app/app/src/main/java/com/example/cardapp/MainActivity.kt
+++ card app/app/src/main/java/com/example/cardapp/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.cardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cardapp.ui.theme.CardAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CreateCard()
                 }
             }
         }
@@ -34,10 +43,72 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun CreateCard() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(
+                corner = CornerSize(15.dp)
+            ),
+            elevation = 4.dp
+        ) {
+
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CreateImageProfile()
+                Divider(modifier = Modifier
+                    .fillMaxWidth(),
+                    startIndent = 12.dp
+                )
+
+                Column(modifier = Modifier.padding(5.dp)) {
+                    Text(text = "Sushant Kandel")
+                    
+                }
+
+            }
+
+        }
+    }
+
+}
+
+@Composable
+private fun CreateImageProfile(modifier:Modifier=Modifier) {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.1.dp, Color.LightGray),
+        elevation = 4.dp
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "profile pic image",
+            modifier = Modifier.size(150.dp),
+            contentScale = ContentScale.FillBounds
+        )
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CardAppTheme {
-        Greeting("Android")
+        CreateCard()
     }
 }
