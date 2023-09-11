10d8cad5bda263982e000efab0838bed63da1aa1
VAI <sushant.kandel@swifttech.com.np>
2023-09-11 14:29:50 +0545

    remember state in compose

--- Compose_Intro/app/src/main/java/com/example/composeintro/MainActivity.kt
+++ Compose_Intro/app/src/main/java/com/example/composeintro/MainActivity.kt
@@ -3,41 +3,97 @@ package com.example.composeintro
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composeintro.ui.theme.ComposeIntroTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeIntroTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+
+    var moneyCounter by remember {
+        mutableStateOf(0)
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        color = Color(0xFF546E7A)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "$$moneyCounter", style = androidx.compose.ui.text.TextStyle(
+                    color = Color.White,
+                    fontSize = 29.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(120.dp))
+            CreateCircle(moneyCount = moneyCounter) { newValue ->
+                moneyCounter = newValue
+            }
+        }
+
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateCircle(
+    moneyCount: Int = 0, updateMoneyCounter: (Int) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(10.dp)
+            .size(105.dp)
+            .clickable {
+                updateMoneyCounter(moneyCount+1)
+            },
+        shape = CircleShape,
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(), contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Tap")
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeIntroTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
